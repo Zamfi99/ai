c478ff29271ff09f2c175768ccf75abba2c37a09
Zamfi99 <andrei.zamfira14@yahoo.com>
2024-05-31 23:03:23 +0300

    Create new AssistantResponse function to work with older versions of next

--- packages/core/streams/assistant-response.ts
+++ packages/core/streams/assistant-response.ts
@@ -2,56 +2,57 @@ import { type AssistantStream } from 'openai/lib/AssistantStream';
 import { Run } from 'openai/resources/beta/threads/runs/runs';
 import { formatStreamPart } from '../shared/stream-parts';
 import { AssistantMessage, DataMessage } from '../shared/types';
+import { Readable } from 'stream';
 
 /**
-You can pass the thread and the latest message into the `AssistantResponse`. This establishes the context for the response.
+ You can pass the thread and the latest message into the `AssistantResponse`. This establishes the context for the response.
  */
 type AssistantResponseSettings = {
   /**
-The thread ID that the response is associated with.
+   The thread ID that the response is associated with.
    */
   threadId: string;
 
   /**
-The ID of the latest message that the response is associated with.
- */
+   The ID of the latest message that the response is associated with.
+   */
   messageId: string;
 };
 
 /**
-The process parameter is a callback in which you can run the assistant on threads, and send messages and data messages to the client.
+ The process parameter is a callback in which you can run the assistant on threads, and send messages and data messages to the client.
  */
 type AssistantResponseCallback = (options: {
   /**
-@deprecated use variable from outer scope instead.
+   @deprecated use variable from outer scope instead.
    */
   threadId: string;
 
   /**
-@deprecated use variable from outer scope instead.
+   @deprecated use variable from outer scope instead.
    */
   messageId: string;
 
   /**
-Forwards an assistant message (non-streaming) to the client.
+   Forwards an assistant message (non-streaming) to the client.
    */
   sendMessage: (message: AssistantMessage) => void;
 
   /**
-Send a data message to the client. You can use this to provide information for rendering custom UIs while the assistant is processing the thread.
- */
+   Send a data message to the client. You can use this to provide information for rendering custom UIs while the assistant is processing the thread.
+   */
   sendDataMessage: (message: DataMessage) => void;
 
   /**
-Forwards the assistant response stream to the client. Returns the `Run` object after it completes, or when it requires an action.
+   Forwards the assistant response stream to the client. Returns the `Run` object after it completes, or when it requires an action.
    */
   forwardStream: (stream: AssistantStream) => Promise<Run | undefined>;
 }) => Promise<void>;
 
 /**
-The `AssistantResponse` allows you to send a stream of assistant update to `useAssistant`.
-It is designed to facilitate streaming assistant responses to the `useAssistant` hook.
-It receives an assistant thread and a current message, and can send messages and data messages to the client.
+ The `AssistantResponse` allows you to send a stream of assistant update to `useAssistant`.
+ It is designed to facilitate streaming assistant responses to the `useAssistant` hook.
+ It receives an assistant thread and a current message, and can send messages and data messages to the client.
  */
 export function AssistantResponse(
   { threadId, messageId }: AssistantResponseSettings,
@@ -159,6 +160,98 @@ export function AssistantResponse(
 }
 
 /**
-@deprecated Use `AssistantResponse` instead.
+ The `AssistantReadableResponse` allows you to send a stream of assistant updates to `useAssistant`.
+ It is designed to facilitate streaming assistant responses to the `useAssistant` hook.
+ It receives an assistant thread and a current message, and can send messages and data messages to the client.
+ This version uses Node.js `Readable` stream instead of `ReadableStream`.
+ This needs to be here in order to work with older versions of Next.js (version 12 and lower).
+ */
+export function AssistantReadableResponse(
+  { threadId, messageId, response }: AssistantResponseSettings & {response: any},
+  process: AssistantResponseCallback,
+) {
+  const stream = new Readable({
+    read() {},
+  });
+
+  const textEncoder = new TextEncoder();
+  const sendMessage = message => {
+    stream.push(
+      textEncoder.encode(formatStreamPart('assistant_message', message)),
+    );
+  };
+  const sendDataMessage = message => {
+    stream.push(textEncoder.encode(formatStreamPart('data_message', message)));
+  };
+  const sendError = errorMessage => {
+    stream.push(textEncoder.encode(formatStreamPart('error', errorMessage)));
+  };
+  const forwardStream = async inputStream => {
+    let result = undefined;
+    for await (const value of inputStream) {
+      switch (value.event) {
+        case 'thread.message.created': {
+          stream.push(
+            textEncoder.encode(
+              formatStreamPart('assistant_message', {
+                id: value.data.id,
+                role: 'assistant',
+                content: [{ type: 'text', text: { value: '' } }],
+              }),
+            ),
+          );
+          break;
+        }
+        case 'thread.message.delta': {
+          const content = value.data.delta.content?.[0];
+          if (content?.type === 'text' && content.text?.value != null) {
+            stream.push(
+              textEncoder.encode(formatStreamPart('text', content.text.value)),
+            );
+          }
+          break;
+        }
+        case 'thread.run.completed':
+        case 'thread.run.requires_action': {
+          result = value.data;
+          break;
+        }
+      }
+    }
+    return result;
+  };
+
+  // Send the threadId and messageId as the first message:
+  stream.push(
+    textEncoder.encode(
+      formatStreamPart('assistant_control_data', {
+        threadId,
+        messageId,
+      }),
+    ),
+  );
+
+  (async () => {
+    try {
+      await process({
+        threadId,
+        messageId,
+        sendMessage,
+        sendDataMessage,
+        forwardStream,
+      });
+    } catch (error) {
+      sendError(error.message ?? `${error}`);
+    } finally {
+      stream.push(null); // Signal the end of the stream
+    }
+  })();
+
+  response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  stream.pipe(response);
+}
+
+/**
+ @deprecated Use `AssistantResponse` instead.
  */
 export const experimental_AssistantResponse = AssistantResponse;
